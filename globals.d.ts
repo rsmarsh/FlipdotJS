2ff28df1d3b8dc483b26a3142bc2fc66d1f04c38
rsmarsh <missingmarshall@gmail.com>
2023-11-25 16:28:57 +0000

    convert to TS

---
+++ globals.d.ts
@@ -0,0 +1,1 @@
+declare module 'flipdot-display';
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import FlipDot from 'flipdot-display';
 
-const PORT = 'COM3';
+const PORT = '/dev/ttyUSB0';
 const ADDRESS = 1;
 const HEIGHT = 16;
 const WIDTH = 96;
@@ -9,6 +9,18 @@ const flippy = new FlipDot(PORT, ADDRESS, HEIGHT, WIDTH);
 
 
 flippy.once('open', function () {
+  console.log("flipdot open")
   flippy.writeText('Hello World!');
   flippy.send();
+  flippy.close();
 });
+
+flippy.once('error', function(err: any) {
+  console.log(err)
+  console.log("an error")
+})
+
+
+flippy.once('close', function() {
+  console.log("closed")
+})
