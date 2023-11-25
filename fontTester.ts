7b9967a9483a42aea16a9bef9f6d34e639f4fd53
rsmarsh <missingmarshall@gmail.com>
2023-11-25 16:50:35 +0000

    TS node changes

---
+++ fontTester.ts
@@ -0,0 +1,34 @@
+import FlipDot from 'flipdot-display';
+import fontList  from './fontlist.js'
+const PORT = 'COM3';
+const ADDRESS = 1;
+const HEIGHT = 16;
+const WIDTH = 96;
+
+const flippy = new FlipDot(PORT, ADDRESS, HEIGHT, WIDTH);
+const message = "Hello World!";
+
+const cycleThroughFonts = () => {
+    let fontIndex = 0;
+
+    const cycle = setInterval(() => {
+        console.log("prepping font ", fontList[fontIndex])
+        showNewFont(fontList[fontIndex]);
+        fontIndex+=1;
+    }, 1000)
+}
+
+const showNewFont = (fontName) => {
+    console.log(`up next: ${fontName}`)
+    flippy.writeText('Hello World!', { font: fontName, width:WIDTH} );
+    flippy.send(); 
+}
+
+flippy.once('open', function () {
+    cycleThroughFonts()
+});
+
+
+// alligator
+
+// 
--- index.ts
+++ index.ts
@@ -1,26 +1,51 @@
 import FlipDot from 'flipdot-display';
 
-const PORT = '/dev/ttyUSB0';
+// const PORT = process.env.PORT;
+// const PORT = '/dev/ttyUSB0';
+// const ADDRESS = process.env.ADDRESS;
+// const ROWS = Number(process.env.ROWS);
+// const COLUMNS = Number(process.env.COLUMNS);
+
+// TODO: move to .env file
+const PORT = 'COM3';
 const ADDRESS = 1;
-const HEIGHT = 16;
-const WIDTH = 96;
+const ROWS = 16;
+const COLUMNS = 96;
 
-const flippy = new FlipDot(PORT, ADDRESS, HEIGHT, WIDTH);
+console.log(process.env);
 
+const flippy = new FlipDot(PORT, ADDRESS, ROWS, COLUMNS);
 
 flippy.once('open', function () {
-  console.log("flipdot open")
+  console.log('flipdot open');
   flippy.writeText('Hello World!');
   flippy.send();
   flippy.close();
 });
 
-flippy.once('error', function(err: any) {
-  console.log(err)
-  console.log("an error")
-})
+flippy.once('error', function (err: any) {
+  console.log(err);
+  console.log('an error');
+});
 
+flippy.once('close', function () {
+  console.log('closed');
+});
 
-flippy.once('close', function() {
-  console.log("closed")
-})
+console.log(process.title);
+
+flippy.once('open', function () {
+  console.log(
+    flippy.writeText(' Hello World!', {
+      font: 'Banner',
+      width: COLUMNS,
+      printDirection: 0
+    })
+  );
+
+  flippy.send();
+});
+
+flippy.once('error', function (err) {
+  console.log('error');
+});
