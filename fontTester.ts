29dd9060b7d87bf062ed5842aa82dba79dbba827
rsmarsh <missingmarshall@gmail.com>
2023-11-25 17:45:10 +0000

    added font cycler test program

--- fontTester.ts
+++ fontTester.ts
@@ -1,34 +1,32 @@
 import FlipDot from 'flipdot-display';
-import fontList  from './fontlist.js'
+import fontList from './fontlist';
+
 const PORT = 'COM3';
 const ADDRESS = 1;
 const HEIGHT = 16;
 const WIDTH = 96;
 
 const flippy = new FlipDot(PORT, ADDRESS, HEIGHT, WIDTH);
-const message = "Hello World!";
 
 const cycleThroughFonts = () => {
-    let fontIndex = 0;
+  let fontIndex = 0;
 
-    const cycle = setInterval(() => {
-        console.log("prepping font ", fontList[fontIndex])
-        showNewFont(fontList[fontIndex]);
-        fontIndex+=1;
-    }, 1000)
-}
+  const cycle = setInterval(() => {
+    console.log('prepping font ', fontList[fontIndex]);
+    showNewFont(fontList[fontIndex]);
+    fontIndex += 1;
+  }, 5000);
+};
 
 const showNewFont = (fontName) => {
-    console.log(`up next: ${fontName}`)
-    flippy.writeText('Hello World!', { font: fontName, width:WIDTH} );
-    flippy.send(); 
-}
+  console.log(`up next: ${fontName}`);
+  flippy.writeText('Hello World!', {
+    font: fontName,
+    width: WIDTH
+  });
+  flippy.send();
+};
 
 flippy.once('open', function () {
-    cycleThroughFonts()
+  cycleThroughFonts();
 });
-
-
-// alligator
-
-// 
