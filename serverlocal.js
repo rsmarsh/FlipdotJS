fce5106acf0d4f2b1272d31208e7fa5fc8be51fe
rsmarsh <missingmarshall@gmail.com>
2024-04-01 19:18:52 +0100

    changed server to accept a matrix

--- serverlocal.js
+++ serverlocal.js
@@ -1,34 +1,31 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const cors = require('cors');
 const serverPort = 3000;
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
 require('dotenv').config();
 
 const { insertMessage, queryMessageHistory } = require('./db.js');
 
-const https = require("https");
-
-const PORT = "/dev/ttyUSB0";
+const PORT = '/dev/ttyUSB0';
 // const PORT = 'COM3'; // Windows USB port
 const ADDRESS = 1;
 const ROWS = 16;
 const COLUMNS = 96;
 
-const FlipDot = require("flipdot-display");
+const FlipDot = require('flipdot-display');
 const flipdot = new FlipDot(PORT, ADDRESS, ROWS, COLUMNS);
 
 let visitorCount = 0;
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   const options = {
-    root: path.join(__dirname),
+    root: path.join(__dirname)
   };
 
-
   res.sendFile(`${options.root}/src/index.html`);
-  console.log(`Visitor ${visitorCount} appeared`)
+  console.log(`Visitor ${visitorCount} appeared`);
   visitorCount += 1;
 });
 
@@ -42,31 +39,33 @@ var allowedOrigins = [
   'https://richardmarshall.dev'
 ];
 
-app.use(cors({
-  origin: function(origin, callback){
-    if(allowedOrigins.indexOf(origin) === -1 && origin !== undefined){
-      console.log(origin)
-      var msg = 'Not in CORS allow list';
-      return callback(new Error(msg), false);
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) === -1 && origin !== undefined) {
+        console.log(origin);
+        var msg = 'Not in CORS allow list';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
     }
-    return callback(null, true);
-  }
-}));
+  })
+);
 
-app.post("/text/", (req, res) => {
+app.post('/text/', (req, res) => {
   const { message, password, font } = req.body;
   const apiPassword = process.env.TEXT_API_PASSWORD;
-    
+
   // return an error if the incorrect password is sent
   if (password !== apiPassword) {
     res.status(401).json({ err: 'Incorrect Password' });
-    return
+    return;
   }
-  
+
   flipdot.writeText(message, {
     font: font,
     width: COLUMNS,
-    printDirection: 0,
+    printDirection: 0
   });
 
   flipdot.send();
@@ -76,41 +75,60 @@ app.post("/text/", (req, res) => {
   insertMessage(message, font);
 });
 
+// allows a matrix to be sent directly to the api, as a true/false array of arrays
+app.post('/matrix/', (req, res) => {
+  const { matrix, password } = req.body;
+
+  // return an error if the incorrect password is sent
+  if (password !== apiPassword) {
+    res.status(401).json({ err: 'Incorrect Password' });
+    return;
+  }
+
+  if (!Array.isArray(matrix)) {
+    res.status(400).json({ err: 'Invalid Matrix' });
+    return;
+  }
+
+  // converts the matrix to bytes then writes it
+  flipdot.writeMatrix(matrix);
+  flipdot.send();
+});
+
 // returns the message list history when requested
 app.get('/history', async (req, res) => {
   const messageHistory = await queryMessageHistory();
   res.json(messageHistory);
-})
+});
 
 app.listen(serverPort, () => {
-  console.log("https server listening");
+  console.log('https server listening');
 });
 
-
-flipdot.once("error", function (err) {
+flipdot.once('error', function (err) {
   console.log(err);
-  console.log("an error occured");
+  console.log('an error occured');
 });
 
-flipdot.once("close", function () {
-  console.log("connection closed");
+flipdot.once('close', function () {
+  console.log('connection closed');
 });
 
-flipdot.once("open", function () {
-  const dataSent = flipdot.writeText("Hello", {
-    font: "Banner",
+flipdot.once('open', function () {
+  const dataSent = flipdot.writeText('Hello', {
+    font: 'Banner',
     width: COLUMNS,
-    printDirection: 0,
+    printDirection: 0
   });
   flipdot.send();
 });
 
 function saveFont(fontName, message) {
-  fs.writeFile("file.txt", data, (err) => {
+  fs.writeFile('file.txt', data, (err) => {
     if (err) {
       throw err;
     }
 
-    console.log("Data has been written to file successfully.");
+    console.log('Data has been written to file successfully.');
   });
 }
